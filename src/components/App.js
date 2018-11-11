cf4099537a41ab2dc81e4d6d1185e3e7d6f65fa6
Tom Workman <tworkman512@gmail.com>
2018-11-11 16:18:13 -0600

    separate out react components

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,32 @@
-import React, { Component } from 'react';
-class App extends Component {
+import React from 'react'
+import ConnectedTodos from './Todos'
+import ConnectedGoals from './Goals'
+import { connect } from 'react-redux'
+import {
+  handleInitialData
+} from '../actions/shared'
+
+class App extends React.Component {
+  componentDidMount() {
+    const { dispatch } = this.props
+
+    dispatch(handleInitialData())
+  }
+
   render() {
+    if (this.props.loading === true) {
+      return <h3>Loading...</h3>
+    }
+
     return (
-      <div className="App">
-        <div>
-        Hello world!
-        </div>
+      <div>
+        <ConnectedTodos />
+        <ConnectedGoals />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default connect((state) => ({
+  loading: state.loading
+}))(App)
