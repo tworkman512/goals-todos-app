334f9e4506d0f40e0a6357f067da16bed2da399d
Tom Workman <tworkman512@gmail.com>
2018-11-11 16:03:06 -0600

    separate out reducers for todos, goals, and loading

---
+++ src/reducers/loading.js
@@ -0,0 +1,13 @@
+import {
+  RECEIVE_DATA
+} from '../actions/shared'
+
+// Loading Indicator Reducer
+export function loading(state = true, action) {
+  switch(action.type) {
+    case RECEIVE_DATA :
+      return false
+    default :
+      return state
+  }
+}
