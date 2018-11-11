48673674c3c233a57a971b2e847bac4a4393c209
Tom Workman <tworkman512@gmail.com>
2018-11-11 15:54:09 -0600

    make actions folder and separate out action creators

---
+++ src/actions/goals.js
@@ -0,0 +1,43 @@
+import API from 'goals-todos-api'
+
+export const ADD_GOAL = 'ADD_GOAL'
+export const REMOVE_GOAL = 'REMOVE_GOAL'
+
+function addGoal(goal) {
+  return {
+    type: ADD_GOAL,
+    goal,
+  }
+}
+
+export function removeGoal(id) {
+  return {
+    type: REMOVE_GOAL,
+    id,
+  }
+}
+
+export function handleAddGoal(name, cb) {
+  return (dispatch) => {
+    return API.saveGoal(name)
+      .then((goal) => {
+        dispatch(addGoal(goal))
+        cb()
+      })
+      .catch(() => {
+        alert('There was an error adding this goal. Please try again.')
+      })
+  }
+}
+
+export function handleDeleteGoal(goal) {
+  return (dispatch) => {
+    dispatch(removeGoal(goal.id))
+
+    return API.deleteGoal(goal.id)
+      .catch(() => {
+        dispatch(addGoal(goal))
+        alert('An error occurred. Please try again.')
+      })
+  }
+}
